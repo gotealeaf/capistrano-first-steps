850c1c7a525b0e2cb48e4c5adde8cc11ad1d4d1d
Juan Lebrijo <juan@lebrijo.com>
2014-07-17 19:20:17 +0200

    Step 7. Installing Thin in your project

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+gem 'thin'
 
 group :development do
   gem 'capistrano-rails', group: :development
--- config/deploy.rb
+++ config/deploy.rb
@@ -40,8 +40,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute "service thin restart"  ## -> line you should add
     end
   end
 
